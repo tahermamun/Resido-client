8682813839c8e07ac390f00ac4fdb4301865da9f
tahermamun <tahermamun5@gmail.com>
2021-05-28 18:11:01 +0600

    2nd commit

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.scss'
+
+
+const Home = () => {
+    return (
+        <div className='main-container'>
+           <span>
+mamaun
+           <h1>This Resido</h1>
+           </span>
+        </div>
+    );
+};
+
+export default Home;
